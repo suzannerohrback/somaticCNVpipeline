60803cfbc0ca227f1edf5452790df881ecd37bff
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-01-29 09:54:29 -0800

    Create runpreprocess.py

---
+++ bin/runpreprocess.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import os
+import shutil
+
+import preprocess
+from common import daemon
+
+
+
+def runAll(args):
+	#print info message
+	
+	#make sure filepaths are in expected format
+	
+	#get list of fastq files to process (depending on args.samples)
+	
+	#use the daemon to and preprocessing code to trim all fastq files with parallel processing
+	
+	#either delete or move full length files to a new folder (depending on args.remove)
+	
+	#print completion message
+
+
+  return 0
