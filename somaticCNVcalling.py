c515cf88a74880fb886393559b12855047193bbe
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-01-29 09:23:42 -0800

    Update somaticCNVcalling.py
    Added command to parse command line arguments
    Added command to initiate preprocessing code

--- somaticCNVcalling.py
+++ somaticCNVcalling.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python
+import sys
+
+import arguments
+
+
+
+
+
+if __name__ == '__main__':
+	objective, args=arguments.fullParser(sys.argv[1:])
+	
+	if objective == 'preprocess':
+		import preprocess
+		runpreprocess.runAll(args)
