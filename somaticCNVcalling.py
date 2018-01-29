7d491f0673b76c896ccd4668faa0cfcf29f38f25
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-01-29 09:49:56 -0800

    Update somaticCNVcalling.py

--- somaticCNVcalling.py
+++ somaticCNVcalling.py
@@ -7,9 +7,14 @@ import arguments
 
 
 
+
+
+
+
+
 if __name__ == '__main__':
 	objective, args=arguments.fullParser(sys.argv[1:])
 	
 	if objective == 'preprocess':
-		import preprocess
+		import runpreprocess
 		runpreprocess.runAll(args)
