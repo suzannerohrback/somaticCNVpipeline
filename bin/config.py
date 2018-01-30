ad3d43813a5c29811a2aca000268f944ed754498
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-01-30 09:34:06 -0800

    Create config.py

---
+++ bin/config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+
+
+"""
+This code can be modified to suit your specific environment
+  Such as file locations
+  Execultable locations
+  
+PLEASE DO NOT SEND ME PULL REQUESTS INCLUDING ANY MODIFICATIONS YOU MAKE TO THIS FILE, I WILL REJECT THEM
+  If I have missed adding in a specific reference, let me know by message
+"""
+
+
+
