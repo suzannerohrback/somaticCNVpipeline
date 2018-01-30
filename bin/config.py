773e39398886b0fc2a87c4d74c38170825f87304
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-01-30 10:21:36 -0800

    Update config.py
    added configuration information for mapping functions

--- bin/config.py
+++ bin/config.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-
+import os
+import inspect
 
 
 """
@@ -10,6 +11,21 @@ This code can be modified to suit your specific environment
 PLEASE DO NOT SEND ME PULL REQUESTS INCLUDING ANY MODIFICATIONS YOU MAKE TO THIS FILE, I WILL REJECT THEM
   If I have missed adding in a specific reference, let me know by message
 """
+
+
+class Map:
+	
+	def __init__(self):
+		self.currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+		
+		self.indexDict =	{
+							'hg38': self.currentdir + '/reference/hg38_index/hg38.index',
+							'mm10': self.currentdir + '/referencemm10_index/mm10.index',
+							}
+		
+		self.samtools = '/home/k4zhang/softwares/samtools-0.1.19/samtools'
+		
+###		self.bowtie = 
 
 
 
