bda2ce400d1025d2572005869b8a9a5aa3abc19e
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-02-01 09:31:09 -0800

    Update runmap.py
    increased versatility for where mapping statistics are saved

--- bin/runmap.py
+++ bin/runmap.py
@@ -26,13 +26,16 @@ def runAll(args):
 	#set up environment#
 	args.FastqDirectory = common.fixDirName(args.FastqDirectory)
 	
-	if not args.output:
-		samDir = os.path.dirname(args.FastqDirectory) + '/' + Sam + '/'
-	else:
+	samDir = os.path.dirname(args.FastqDirectory[:-1]) + '/' + Sam + '/'
+	if args.output:
 		samDir = common.fixDirName(args.output)
 	
+	statsDir = os.path.dirname(args.FastqDirectory[:-1]) + '/' + PipelineStats + '/'
+	if args.statdir:
+		statsDir = common.fixDirName(args.statdir)
+
 	tempDir = args.FastqDirectory + 'Temp/'
-	statsDir = args.FastqDirectory + 'MapStats/'
+
 	for i in [samDir, tempDir, statsDir]:
 		common.makeDir(i)		
 
