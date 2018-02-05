d39f791b82b299e209b6f97bfbcacb431f602f41
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-02-05 09:51:33 -0800

    Update config.py
    Created class for segmentation function variables

--- bin/config.py
+++ bin/config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import inspect
+import sys
 
 
 """
@@ -76,3 +77,30 @@ class Count:
 
 
 
+
+		
+		
+		
+class Segment:
+	
+	def __init__(self):
+		self.currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+		
+		self.binDict =	{
+						'hg38': self.currentdir + '/reference/hg38.varbin.fullRef.25k.bowtie.k36.txt',
+						'mm10': self.currentdir + '/reference/mm10.varbin.fullRef.25k.bowtie.k36.txt',
+						}
+		
+	#	self.chromDict =	{
+	#						'hg38': self.currentdir + '/reference/hg38.chrom.sizes.txt',
+	#						'mm10': self.currentdir + '/reference/mm10.chrom.sizes.txt',
+	#						}
+	
+		self.statsmodelsLocation = '/home/srohrbac/research/packages/statsmodels-0.6.1'
+		sys.path.append(self.statsmodelsLocation)
+		
+		
+		
+		
+		
+		
