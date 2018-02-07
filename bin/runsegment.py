f7c18af9ed173ebfd10f1cbb401d25958151025b
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-02-07 11:17:19 -0800

    Update runsegment.py
    Added call to run CBS segmentation in matlab after normalization

--- bin/runsegment.py
+++ bin/runsegment.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import numpy as np
+import shutil
 
 import common
 from segment import normalizefile, segmentfile
@@ -47,10 +48,6 @@ def runAll(args):
 		
 		
 		
-		
-		
-	#Use multiprocessing daemon to run normalization for all samples#
-	
 	info = common.importInfoFile(args.infofile, args.columns, 'normalize')
 	
 	if args.infofile:
@@ -61,7 +58,14 @@ def runAll(args):
 			[ (basename(x)[:-14], 'unk', 1,) for x in sampleFiles],
 			dtype=thisDtype)
 		
-	sampleDict = {x: [y for y in sampleFiles if x in y][0] for x in refArray['name']}
+	sampleDict = {x: [y for y in sampleFiles if x == os.path.basename(y)[:len(x)]][0] for x in refArray['name']}
+
+	
+	
+	
+	
+	#Run normalization for all samples#
+	
 		
 		
 	methodDict = {x: False for x in np.unique(refArray['method'])}	
@@ -84,8 +88,8 @@ def runAll(args):
 		
 		
 	#run multiprocessing for gc (+ method) correction
-	argList = [(args.species, sampleDict[x], methodDict[sampleNormMethodDict[x]], lowessDir + x + '.lowess.txt') for x in sampleDict]
-	common.daemon(normalizefile.runNormalizeOne, argList, 'normalize bincount files')
+	normArgs = [(args.species, sampleDict[x], methodDict[sampleNormMethodDict[x]], lowessDir + x + '.lowess.txt') for x in sampleDict.keys()]
+	common.daemon(normalizefile.runNormalizeOne, normArgs, 'normalize bincount files')
 
 	
 	
@@ -98,21 +102,12 @@ def runAll(args):
 		
 		
 		
-	#Run segmentation#
-	#write matlab script
-	#run matlab script
-	#run for all samples in parallel with multiprocessing daemon
+	#Run CBS for all samples#
+	segArgs = [(x, args.species, tempDir, lowessDir, segmentDir) for x in refArray['name']]
+	common.daemon(segmentfile.segmentOne, segArgs, 'segment bincount data')
 	
-	
-	
-	
-	
-	printText = '\nTHIS FUNCTION IS NOT YET COMPLETE, SORRY\n'
-	print(printText)
-	raise SystemExit
-	
+	shutil.rmtree(tempDir[:-1])
 
-	
 	print('\nSegmentation complete\n\n\n')
 
 	
