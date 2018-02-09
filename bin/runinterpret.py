80621a6c8b83acfec84c0595e4fad5d2870042b6
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-02-09 09:41:46 -0800

    Create runinterpret.py
    created file, but doesn't do anything yet

---
+++ bin/runinterpret.py
@@ -0,0 +1,21 @@
+#!usr/bin/python
+
+
+
+
+
+
+
+
+
+
+def runAll(args):
+
+	errorText = 'SORRY, THIS FUNCTION IS STILL BEING WRITTEN, TRY AGAIN LATER\n\n\n'
+	print(errorText)
+	raise SystemExit
+	
+	
+	
+	
+	
