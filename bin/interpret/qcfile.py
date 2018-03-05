5906c79526d97519644f6dfcd6bede758768e14e
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-03-05 11:05:37 -0800

    Create qcfile.py
    outlined code I need to write

---
+++ bin/interpret/qcfile.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+
+
+
+
+def readCount():
+	return 0
+	
+	
+	
+def MAPD():
+	return 0
+	
+	
+	
+def CS():
+	return 0
+	
+	
+	
+def getPloidy():
+	return 0
+
+
+
+def getGender():
+	return 0
+
+
+
+
+def runOne():
+	#import config info
+
+	#determine read counts from the countstats file
+	
+	#determine MAPD from lowess bins
+	
+	#determine the optimal ploidy value
+	
+	#determine CS
+	
+	#determine gender
+	
+	#determine if the sample is good
+	
+	
+	
+	#write an intermediate file with name, reads, MAPD, CS, ploidy, gender, good/bad
+
+	return 0
