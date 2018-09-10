73f89b1ea886d880682ab6a3158c484241798473
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-09-09 18:01:55 -0700

    Update common.py

--- bin/common.py
+++ bin/common.py
@@ -85,7 +85,8 @@ def getSampleList(folder, sampleArg, extension):
 def importInfoFile(infoFile, columns, useFunction, skiprows=0):
 	functionDict =	{
 					'normalize': {'names': ('name', 'method', 'cells'), 'formats': ('S50', 'S50', 'int')},
-					'normref': {'names': ('chrom', 'chrStart', 'abspos', 'size', 'gc'), 'formats': ('S10', 'int', 'int', 'int', 'float')},
+					'normref': {'names': ('chrom', 'chrStart', 'abspos', 'size', 'gc'), 
+								'formats': ('S10', 'int64', 'int64', 'int64', 'float64')},
 					'interpret': {'names': ('name', 'cells', 'group'), 'formats': ('S50', 'int', 'S50')}
 					}
 	
