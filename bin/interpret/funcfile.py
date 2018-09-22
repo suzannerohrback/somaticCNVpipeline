495faed7737a5417c33a19ae5694904ac582c97f
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-09-22 16:32:25 -0700

    Update funcfile.py

--- bin/interpret/funcfile.py
+++ bin/interpret/funcfile.py
@@ -171,7 +171,7 @@ def FUnCone(sample, species, segmentDir, CNVdir, ploidy, gender):
 	
 	
 	#load segment data#
-	segData, segArray = common.importSegData(sample, segmentDir, binArray)
+	segData, segArray = common.importSegData(sample, segmentDir, refArray)
 	segData['CN'] = segData['CN'] * ploidy
 	dataDict = [ {y: x[y] for y in segData.dtype.names} for x in segData ]
 
