b3136f7126ff72a31989c7b5ce1c9030a2b94bf2
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-09-22 19:16:56 -0700

    Update funcfile.py

--- bin/interpret/funcfile.py
+++ bin/interpret/funcfile.py
@@ -310,7 +310,7 @@ def FUnCone(sample, species, segmentDir, CNVdir, ploidy, gender):
 		OUT.write('\t')
 		OUT.write(str(refArray[binDict[i['start']]]['chrStart']))
 		OUT.write('\t')
-		OUT.write(str(refArray[binDict[i['end']+1]]['chrStart']-1))
+		OUT.write(str(refArray[binDict[i['end']+1]-1]['chrStart'] + refArray[binDict[i['end']+1]-1]['size'] -1))
 		OUT.write('\t')
 		OUT.write(str(np.round(i['CN'])))
 		OUT.write('\n')
