935c1f207ffb56181e7be66ba134bf06d341505f
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-09-22 19:23:03 -0700

    Update analyzefiles.py

--- bin/interpret/analyzefiles.py
+++ bin/interpret/analyzefiles.py
@@ -9,6 +9,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
 import config as cfg
+import common
 
 
 
