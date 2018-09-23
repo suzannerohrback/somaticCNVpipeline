62ef2f6fc09efd778577649ca66fd286177df139
Suzanne Rohrback <suzanne.rohrback@gmail.com>
2018-09-23 13:57:32 -0700

    Update EXAMPLE_SHELL_COMMANDS.sh

--- EXAMPLE_SHELL_COMMANDS.sh
+++ EXAMPLE_SHELL_COMMANDS.sh
@@ -9,15 +9,15 @@ fastqDir=/oasis/tscc/scratch/srohrbac/research/pipelinetest2/Fastq/
 species=mm10
 mapidx=/home/srohrbac/research/reference_files/mappingRef/mm10_index/mm10.index
 pipelineDir=/home/srohrbac/research/scripts/GITtmp/somaticCNVpipeline
-
+samtools=/home/k4zhang/softwares/samtools-0.1.19/samtools
 
 
 cd $pipelineDir
 
 python somaticCNVcalling preprocess $fastqDir -5 30 -l 36
-python somaticCNVcalling map $fastqDir $mapidx
+python somaticCNVcalling map -m $samtools $fastqDir $mapidx
 python somaticCNVcalling count $analysisDir $species
-python somaticCNVcalling $analysisDir $species
-python somaticCNVcalling $analysisDir $species
+python somaticCNVcalling segment $analysisDir $species
+python somaticCNVcalling interpret $analysisDir $species
 
 
